30c722a0558070a3b713c5465dbaa29c7a68479f
vuthanhdat <80867452+vuthanhdatt@users.noreply.github.com>
2021-03-19 14:44:27 +0700

    take input from user

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,20 @@
 import random
 
 def main():
-    dice_roll = 10
+    dice_roll = int(input('How many dice? '))
+    dice_size = int(input('Dice size? '))
     sum_roll = 0
     for i in range(0, dice_roll):    
-        roll = random.randint(1,6)
+        roll = random.randint(1,dice_size)
         sum_roll += roll
         print(f'You random {roll}')
-    print(f'Sum: {sum_roll}')
+        if roll == 1:
+          print(f'You rolled a {roll}! Critical Fail')
+        elif roll == dice_size:
+          print(f'You rolled a {roll}! Critical Success!')
+        else:
+          print(f'You rolled a {roll}')
+          print(f'Sum: {sum_roll}')
 
 if __name__== "__main__":
   main()
