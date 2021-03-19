4b730dcb5daf323bc0b0c04bc9889d8077704e56
vuthanhdat <80867452+vuthanhdatt@users.noreply.github.com>
2021-03-19 14:38:02 +0700

    roll 10 times and sum

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,14 @@
+import random
+
 def main():
-  print('You rolled a die') 
+    dice_roll = 10
+    sum_roll = 0
+    for i in range(0, dice_roll):    
+        roll = random.randint(1,6)
+        sum_roll += roll
+        print(f'You random {roll}')
+    print(f'Sum: {sum_roll}')
 
 if __name__== "__main__":
   main()
+
